e872a990ff522f7a8e9d77e1da4afa40c3cece46
Richard JH McCrae <richard.jh.mccrae@gmail.com>
2023-10-12 09:05:27 +0200

    all 4 LEDs work!

--- recipes-kernel/my-led-driver/files/my_led_driver_bbb.c
+++ recipes-kernel/my-led-driver/files/my_led_driver_bbb.c
@@ -4,50 +4,70 @@
 #include <linux/platform_device.h>
 #include <linux/kobject.h>
 
-static struct gpio_desc *led0 = NULL;
-static struct gpio_desc *led1 = NULL;
-static struct gpio_desc *led2 = NULL;
-static struct gpio_desc *led3 = NULL;
-static int led0_state = 0;
-static int led1_state = 0;
-static int led2_state = 0;
-static int led3_state = 0;
+// static struct gpio_desc *led0 = NULL;
+// static struct gpio_desc *led1 = NULL;
+// static struct gpio_desc *led2 = NULL;
+// static struct gpio_desc *led3 = NULL;
 static struct kobject *led_kobj;
 
-static ssize_t my_module_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+#define NUM_LEDS 4
+
+struct led_attr {
+    int led_state;
+    struct kobj_attribute attr;
+    struct gpio_desc *led;
+};
+
+static ssize_t led_read(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	/* Write current LED state to user buffer */
-	return sprintf(buf, "%d\n", led0_state);
+	// return sprintf(buf, "%d\n", led0_state);
+
+    struct led_attr *led_attribute = container_of(attr, struct led_attr, attr);
+    return sprintf(buf, "%d\n", led_attribute->led_state);
 }
 
-static ssize_t my_module_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+static ssize_t led_write(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
     // Read input and update LED state, user space can update LED state
-	sscanf(buf, "%du", &led0_state);
-	if (led0_state == 1 || led0_state == 0) {
-		gpiod_set_value(led0, led0_state);
+	// sscanf(buf, "%du", &led0_state);
+	// if (led0_state == 1 || led0_state == 0) {
+	// 	gpiod_set_value(led0, led0_state);
+	// }
+	// return count;
+
+    struct led_attr *led_attribute = container_of(attr, struct led_attr, attr);
+
+    sscanf(buf, "%du", &led_attribute->led_state);
+    if (led_attribute->led_state == 1 || led_attribute->led_state == 0) {
+		gpiod_set_value(led_attribute->led, led_attribute->led_state);
 	}
-	return count;
+
+    return count;
 }
 
 /* Create the file with name, "led0_state", specify read and write callbacks */
-static struct kobj_attribute led0_state_attr = __ATTR(led0_state, 0660, my_module_show, my_module_store);
-static struct kobj_attribute led2_state_attr = __ATTR(led1_state, 0660, my_module_show, my_module_store);
-static struct kobj_attribute led3_state_attr = __ATTR(led2_state, 0660, my_module_show, my_module_store);
-static struct kobj_attribute led4_state_attr = __ATTR(led3_state, 0660, my_module_show, my_module_store);
+// static struct kobj_attribute led0_state_attr = __ATTR(led0_state, 0660, my_module_show, my_module_store);
+// static struct kobj_attribute led2_state_attr = __ATTR(led1_state, 0660, my_module_show, my_module_store);
+// static struct kobj_attribute led3_state_attr = __ATTR(led2_state, 0660, my_module_show, my_module_store);
+// static struct kobj_attribute led4_state_attr = __ATTR(led3_state, 0660, my_module_show, my_module_store);
+static struct led_attr led_attrs[NUM_LEDS] = {
+    [0] = { .attr = __ATTR(led0_state, 0660, led_read, led_write) },
+    [1] = { .attr = __ATTR(led1_state, 0660, led_read, led_write) },
+    [2] = { .attr = __ATTR(led2_state, 0660, led_read, led_write) },
+    [3] = { .attr = __ATTR(led3_state, 0660, led_read, led_write) }
+};
 
 
 static int my_module_probe(struct platform_device *pdev)
 {
-	printk("LED: Running device probe\n");
-
-	int err;
-	struct device *dev = &pdev->dev;
-	int db;
+	int err, i;
     const char *label0;
     const char *label1;
     const char *label2;
     const char *label3;
+
+	struct device *dev = &pdev->dev;
 
 	// Fetch all user LEDs
 	struct fwnode_handle *fwnode_btn0 = device_get_named_child_node(dev, "led2");
@@ -55,26 +75,28 @@ static int my_module_probe(struct platform_device *pdev)
 	struct fwnode_handle *fwnode_btn2 = device_get_named_child_node(dev, "led4");
 	struct fwnode_handle *fwnode_btn3 = device_get_named_child_node(dev, "led5");
 
-	// Fetch labels
-	err = fwnode_property_read_string(fwnode_btn0, "labe0", &labe0);
+	printk("LED: Running device probe\n");
+
+	// Fetch labels from am335x-bone-common.dtsi
+	err = fwnode_property_read_string(fwnode_btn0, "label", &label0);
     if (err) {
         printk("Failed to read label property\n");
         return -ENODATA;
     }
 
-	err = fwnode_property_read_string(fwnode_btn1, "label", &label);
+	err = fwnode_property_read_string(fwnode_btn1, "label", &label1);
     if (err) {
         printk("Failed to read label property\n");
         return -ENODATA;
     }
 
-	err = fwnode_property_read_string(fwnode_btn2, "labe2", &labe2);
+	err = fwnode_property_read_string(fwnode_btn2, "label", &label2);
     if (err) {
         printk("Failed to read label property\n");
         return -ENODATA;
     }
 
-	err = fwnode_property_read_string(fwnode_btn3, "labe3", &labe3);
+	err = fwnode_property_read_string(fwnode_btn3, "label", &label3);
     if (err) {
         printk("Failed to read label property\n");
         return -ENODATA;
@@ -82,21 +104,25 @@ static int my_module_probe(struct platform_device *pdev)
     printk("Labels are: %s, %s, %s, %s\n", label0, label1, label2, label3);
 
     // Set up LEDs as GPIO output devices
-	led0 = devm_fwnode_gpiod_get(dev, fwnode_btn0, NULL, GPIOD_OUT_HIGH, label);
-	led1 = devm_fwnode_gpiod_get(dev, fwnode_btn0, NULL, GPIOD_OUT_HIGH, label);
-	led2 = devm_fwnode_gpiod_get(dev, fwnode_btn0, NULL, GPIOD_OUT_HIGH, label);
-	led3 = devm_fwnode_gpiod_get(dev, fwnode_btn0, NULL, GPIOD_OUT_HIGH, label);
-	if(IS_ERR(led0) || IS_ERR(led1) || IS_ERR(led2) ||  IS_ERR(led3)) {
-		printk("Error, could not set up GPIOs\n");
+	led_attrs[0].led = devm_fwnode_gpiod_get(dev, fwnode_btn0, NULL, GPIOD_OUT_HIGH, label0);
+	led_attrs[1].led = devm_fwnode_gpiod_get(dev, fwnode_btn1, NULL, GPIOD_OUT_HIGH, label1);
+	led_attrs[2].led = devm_fwnode_gpiod_get(dev, fwnode_btn2, NULL, GPIOD_OUT_HIGH, label2);
+	led_attrs[3].led = devm_fwnode_gpiod_get(dev, fwnode_btn3, NULL, GPIOD_OUT_HIGH, label3);
+	if(IS_ERR(led_attrs[0].led) ||
+        IS_ERR(led_attrs[1].led) ||
+        IS_ERR(led_attrs[2].led) ||
+        IS_ERR(led_attrs[3].led)) {
+
+        printk("Error, could not set up GPIOs\n");
 		return -EIO;
 	}
 	printk("LEDs set up as GPIO devices\n");
 
 	/* store current LEDs state to HIGH */
-	led0_state = 1;
-	led1_state = 1;
-	led2_state = 1;
-	led3_state = 1;
+	// led0_state = 1;
+	// led1_state = 1;
+	// led2_state = 1;
+	// led3_state = 1;
 
     // Create sysfs entry
     led_kobj = kobject_create_and_add("my_led_sysfs", NULL);
@@ -104,37 +130,12 @@ static int my_module_probe(struct platform_device *pdev)
         printk("Failed to create sysfs entry\n");
         return -ENOMEM;
     }
-    // Add attributes
-    err = sysfs_create_file(led_kobj, &led0_state_attr.attr);
-    if (err) {
-        printk("Failed to create sysfs attribute for LED0\n");
-        // Cleanup sysfs entry
-        kobject_put(led_kobj);
-        return -ENOMEM;
-    }
-
-	err = sysfs_create_file(led_kobj, &led1_state_attr.attr);
-    if (err) {
-        printk("Failed to create sysfs attribute for LED1\n");
-        // Cleanup sysfs entry
-        kobject_put(led_kobj);
-        return -ENOMEM;
-    }
-
-	err = sysfs_create_file(led_kobj, &led2_state_attr.attr);
-    if (err) {
-        printk("Failed to create sysfs attribute for LED2\n");
-        // Cleanup sysfs entry
-        kobject_put(led_kobj);
-        return -ENOMEM;
-    }
-
-    err = sysfs_create_file(led_kobj, &led3_state_attr.attr);
-    if (err) {
-        printk("Failed to create sysfs attribute for LED3\n");
-        // Cleanup sysfs entry
-        kobject_put(led_kobj);
-        return -ENOMEM;
+    // Add attributes, LEDs being in off state
+    for (i = 0; i < NUM_LEDS; i++) {
+        led_attrs[i].led_state = 0;
+        if (sysfs_create_file(led_kobj, &led_attrs[i].attr.attr)) {
+            printk(KERN_ERR "Failed to create led%d_state in /sys/kernel/led\n", i);
+        }
     }
 
     return 0;
@@ -142,11 +143,18 @@ static int my_module_probe(struct platform_device *pdev)
 
 static int my_module_remove(struct platform_device *pdev)
 {
+    int i;
+
     printk("LED: Running device remove\n");
+
     // Turn off LED
-    gpiod_set_value(led0, 0);
-    // Cleanup sysfs entry
-    sysfs_remove_file(led_kobj, &led_state_attr.attr);
+    for (i = 0; i < NUM_LEDS; i++) {
+        gpiod_set_value(led_attrs[i].led, 0);
+        led_attrs[i].led_state = 0;
+
+        sysfs_remove_file(led_kobj, &led_attrs[i].attr.attr);
+    }
+
     kobject_put(led_kobj);
     return 0;
 }
